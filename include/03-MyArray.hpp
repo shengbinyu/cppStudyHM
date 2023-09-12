898ff3639240edbf7eb47c107e00b93b26e48aad
sbyu <yushengbin95@163.com>
2023-09-12 22:54:59 +0800

    Complete 03-MyArray

--- include/03-MyArray.hpp
+++ include/03-MyArray.hpp
@@ -8,7 +8,7 @@ class MyArray
 public:
     MyArray(int capacity)
     {
-        cout << " Construction function with parameters." << endl;
+        //cout << " Construction function with parameters." << endl;
         this->m_Capacity = capacity;
         this->m_Size = 0;
         this->pAddress = new T[this->m_Capacity]; 
@@ -16,20 +16,20 @@ public:
 
     MyArray(const MyArray& arr)
     {
-        cout << " Construction function with Copy." << endl;
+        //cout << " Construction function with Copy." << endl;
         this->m_Capacity = arr.m_Capacity;
         this->m_Size = arr.m_Size;
         this->pAddress = new T[arr.m_Capacity];
 
-        for(int i = 0; i < this->m_Size ; i++)
+        for(int i = 0; i < this->m_Size; i++)
         {
-            this->pAddress = arr.pAddress[i];
+            this->pAddress[i] = arr.pAddress[i];
         }
     }
 
     MyArray& operator=(const MyArray& arr)
     {
-        cout << " Recontruct operator = with deep copy." << endl;
+        //cout << " Recontruct operator = with deep copy." << endl;
         if (this->pAddress != NULL)
         {
             delete [] this->pAddress;
@@ -44,15 +44,48 @@ public:
 
         for(int i = 0; i < this->m_Size ; i++)
         {
-            this->pAddress = arr.pAddress[i];
+            this->pAddress[i] = arr.pAddress[i];
         }
         return *this;
     }
 
+    void push_Back(const T & val)
+    {
+        if ( this->m_Capacity == this->m_Size)
+        {
+            return;
+        }
+        this->pAddress[this->m_Size] = val;
+        this->m_Size++;
+    }
+
+    void pop_Back()
+    {
+        if(this->m_Size == 0)
+        {
+            return;
+        }
+        this->m_Size--;
+    }
+
+    T& operator[] (int index)
+    {
+        return this->pAddress[index];
+    }
+
+    int getCapacity()
+    {
+        return this->m_Capacity;
+    }
+
+    int getSize()
+    {
+        return this->m_Size;
+    }
 
     ~MyArray() 
     {
-        cout << " Destruction function." << endl;
+        //cout << " Destruction function." << endl;
         if (this->pAddress != NULL)
         {
             delete [] this->pAddress;
--- src/03-MyArray.cpp
+++ src/03-MyArray.cpp
@@ -1,17 +1,86 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+class Person
+{
+public:
+     Person(){}
+     Person(string name, int age)
+     {
+         this->m_Name = name;
+         this->m_Age = age;
+     }
+public:
+     string m_Name;
+     int m_Age;
+};
+
 #include "03-MyArray.hpp"
+
+void printArray(MyArray<int> & arr)
+{
+    for(int i =0; i < arr.getSize(); i++)
+    {
+        cout << arr[i] << endl;
+    }
+}
+
+void printPersonArray(MyArray<Person> &arr)
+{
+    for(int i = 0; i < arr.getSize(); i++)
+    {
+        cout << "The name is: " << arr[i].m_Name << ", and the age is : " << arr[i].m_Age<< endl;
+    }
+}
 
 void test01()
 {
     MyArray<int> arr1(5);
-//    MyArray<int> arr2(arr1);
+    // MyArray<int> arr2(arr1);
+    // MyArray<int> arr3(100);
+    for (int i = 0 ;i < 5; i++)
+    {
+        arr1.push_Back(i);
+    }
+    cout << "Pint arr1:" << endl;
+    printArray(arr1);
+    cout << "The capacity of arr1:" << arr1.getCapacity() << endl;
+    cout << "The Size of arr1:" << arr1.getSize() << endl;
+
+    MyArray<int> arr2(arr1);
+    arr2.pop_Back();
+    printArray(arr1);
+    cout << "Pint arr2:" << endl;
+    cout << "The capacity of arr2:" << arr2.getCapacity() << endl;
+    cout << "The Size of arr2:" << arr2.getSize() << endl;
 }
 
+
+void test02()
+{
+    MyArray<Person> arr(10);
+    Person p1("Tom", 99);
+    Person p2("Jack", 48);
+    Person p3("xiaoming", 20);
+    Person p4("NgkK", 19);
+    Person p5("Dacry", 9);
+
+    arr.push_Back(p1);
+    arr.push_Back(p2);
+    arr.push_Back(p3);
+    arr.push_Back(p4);
+    arr.push_Back(p5);
+
+    printPersonArray(arr);
+    cout << "The capacity of arr:" << arr.getCapacity() << endl;
+    cout << "The Size of arr:" << arr.getSize() << endl;
+
+}
 int main()
 {
-    test01();
+    //test01();
+    test02();
     return 0;
 }
 
